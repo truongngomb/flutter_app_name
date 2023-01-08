9d8f7e61efa0c002fd99ce1bb140a334c8f37419
TRUONG NGO <dev.truongngo@gmail.com>
2023-01-08 14:26:25 +0700

    Update package name in android manifest

--- lib/android.dart
+++ lib/android.dart
@@ -22,19 +22,52 @@ String fetchCurrentBundleName(Context context, String manifestFileData) {
   return label[0] as String;
 }
 
+String fetchCurrentPackageName(Context context, String manifestFileData) {
+  final parsed = XmlDocument.parse(manifestFileData);
+
+  final application = parsed.findAllElements("manifest").toList()[0];
+
+  final List<String> label = application.attributes
+      .where((attrib) => attrib.toString().contains("package"))
+      .map((i) => i.toString())
+      .toList();
+
+  if (label.isEmpty) {
+    throw Exception(
+        "Could not find android:label in ${context.androidManifestPath}");
+  }
+
+  return label[0] as String;
+}
+
 String setNewBundleName(Context context, String manifestFileData,
     String currentBundleName, String desiredBundleName) {
   return manifestFileData.replaceAll(
       currentBundleName, 'android:label="${desiredBundleName}"');
 }
 
+String setNewPackageName(Context context, String manifestFileData,
+    String currentPackageName, String desiredPackageName) {
+  return manifestFileData.replaceAll(
+      currentPackageName, 'package="${currentPackageName}"');
+}
+
 void updateLauncherName(Context context) {
   final String manifestFileData = common.readFile(context.androidManifestPath);
+
   final String desiredBundleName = common.fetchLauncherName(context);
   final String currentBundleName =
       fetchCurrentBundleName(context, manifestFileData);
-  final String updatedManifestData = setNewBundleName(
+  String updatedManifestData = setNewBundleName(
       context, manifestFileData, currentBundleName, desiredBundleName);
+
+  final String? desiredPackageName = common.fetchId(context);
+  if (desiredPackageName != null) {
+    final String currentPackageName =
+        fetchCurrentPackageName(context, manifestFileData);
+    updatedManifestData = setNewPackageName(
+        context, manifestFileData, currentPackageName, desiredPackageName);
+  }
 
   common.overwriteFile(context.androidManifestPath, updatedManifestData);
 }
